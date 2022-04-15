3d2ccbebebe9afe7e1683e460b6501bb545798c5
czar <czar.yobero@gmail.com>
2022-04-15 09:04:25 -0700

    implement Retrievable trait

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,26 @@
-use super::models::{NewStrain, Strain};
-use super::schema::strains::dsl::strains;
+use super::models::{NewStrain, Species, Strain};
+use super::schema::strains::dsl::{self, strains};
 
 use diesel::pg::PgConnection;
 use diesel::result::Error;
-use diesel::{Connection, ConnectionError, ExpressionMethods, QueryDsl, Queryable, RunQueryDsl};
+use diesel::sql_types::Integer;
+use diesel::{
+    sql_query, Connection, ConnectionError, ExpressionMethods, QueryDsl, Queryable, RunQueryDsl,
+};
 use dotenv::dotenv;
 use std::env;
+
+pub struct StrainParams<'a> {
+    pub id: Option<i32>,
+    pub name: Option<&'a str>,
+    pub species: Option<Species>,
+}
+
+pub enum StrainField<I = i32, N = String, S = Species> {
+    Id(I),
+    Name(N),
+    Species(S),
+}
 
 /// A Utility function to establish DB connection
 /// Example:
@@ -31,45 +46,66 @@ pub trait Creatable<C = PgConnection, E = Error>
 where
     C: Connection,
 {
-    type Obj;
+    type Output;
 
-    fn create(&self, conn: &C) -> Result<Self::Obj, E>;
+    fn create(&self, conn: &C) -> Result<Self::Output, E>;
 }
 
 /// Trait for deleting objects
 /// Example:
-///     let conn = establish_connection().unwrap();
-///     let new = NewStrain {
-///         name: "Reggie Kush".to_owned(),
-///         species: Species::Indica,
-///     };
-///     let strain = new.create(&conn).unwrap();
-///     assert!(strain.delete(&conn).is_ok());
-///     let fails = strains.find(strain.id).get_result::<Strain>(&conn);
-///     assert!(fails.is_err());
+/// let conn = establish_connection().unwrap();
+/// let new = NewStrain {
+///     name: "Reggie Kush".to_owned(),
+///     species: Species::Indica,
+/// };
+/// let strain = new.create(&conn).unwrap();
+/// assert!(strain.delete(&conn).is_ok());
+/// let fails = strains.find(strain.id).get_result::<Strain>(&conn);
+/// assert!(fails.is_err());
 pub trait Deletable<C = PgConnection, E = Error> {
-    type Obj;
-    fn delete(&self, conn: &C) -> Result<Self::Obj, E>;
+    type Output;
+    fn delete(&self, conn: &C) -> Result<Self::Output, E>;
+}
+
+/// Trait for retrieving objects
+pub trait Retrievable<'a, C = PgConnection, E = Error> {
+    type Field;
+    type Output;
+    fn all(conn: &C) -> Result<Vec<Self::Output>, Error>;
+    fn filter(conn: &C, field: Self::Field) -> Result<Vec<Self::Output>, E>;
 }
 
 impl Creatable for NewStrain {
-    type Obj = Strain;
+    type Output = Strain;
     fn create(&self, conn: &PgConnection) -> Result<Strain, Error> {
         diesel::insert_into(strains).values(self).get_result(conn)
     }
 }
 
 impl Deletable for Strain {
-    type Obj = Strain;
+    type Output = Strain;
     fn delete(&self, conn: &PgConnection) -> Result<Strain, Error> {
         diesel::delete(strains.find(&self.id)).get_result(conn)
+    }
+}
+
+impl Retrievable<'_> for Strain {
+    type Field = StrainField;
+    type Output = Strain;
+    /// Retrieves all objects
+    fn all(conn: &PgConnection) -> Result<Vec<Strain>, Error> {
+        strains.load(conn)
+    }
+
+    fn filter(conn: &PgConnection, field: StrainField) -> Result<Vec<Strain>, Error> {
+        unimplemented!()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::models::{NewStrain, Species};
+    use crate::models::{NewStrain, Species, Strain};
 
     #[test]
     fn connection_established() {
@@ -104,5 +140,12 @@ mod tests {
         assert!(strain.delete(&conn).is_ok());
         let fails = strains.find(strain.id).get_result::<Strain>(&conn);
         assert!(fails.is_err());
+    }
+
+    #[test]
+    fn all_strains_retrieved() {
+        let conn = establish_connection().unwrap();
+        let res = Strain::all(&conn);
+        assert!(res.is_ok());
     }
 }
