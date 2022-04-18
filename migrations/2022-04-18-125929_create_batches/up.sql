34bf9bc5ac6880b9799153804471aa64af5f1a7c
czar <czar.yobero@gmail.com>
2022-04-18 06:10:38 -0700

    create batches table and schema

---
+++ migrations/2022-04-18-125929_create_batches/up.sql
@@ -0,0 +1,16 @@
+-- Your SQL goes here
+CREATE TABLE growers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE batches (
+    id SERIAL PRIMARY KEY,
+    strain_id INT NOT NULL,
+    harvest_date DATE NULL,
+    final_test_date DATE NULL,
+    package_date DATE NULL,
+    grower_id INT NOT NULL,
+    FOREIGN KEY (strain_id) REFERENCES strains (id) ON DELETE CASCADE,
+    FOREIGN KEY (grower_id) REFERENCES growers (id) ON DELETE CASCADE
+);
